b9828709da0957e7b703c0062966c5f1cd179035
Your Name <you@example.com>
2017-12-07 13:19:42 +0100

    comentaris a la funció i al codi

--- funcio.js
+++ funcio.js
@@ -1,10 +1,10 @@
-function primer (numero) {
-    var i = 2;
+function primer (numero) { /*defnició de la funció que enllaçarà la pagina principal*/
+    var i = 2; /*donada una variable, mentre el numero que s'introdueixi sigui menor a la variable inicial, si el residu */
     while (i<numero) {
         if (numero%i==0){
             return false;
         }
-        i++;
+        i++; //anirà afgint un cada vegada fins arribar a la variable inicial (2) //
     }
     return true;
 }
